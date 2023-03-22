639d614bc1af3ce155f7599779bc8ea4edab8c66
Igor Moskalev <moskalev.igor.v@gmail.com>
2023-03-22 12:58:06 +0600

    Base screen MVP with CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component }  from 'react';
+import coldBg from './assets/cold.jpg'
+import hotBg from './assets/hot.jpg'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{backgroundImage: `url(${coldBg})`}}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="City" placeholder="Enter City..." />
+            <button>°F</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="weatherIcon" />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 °C</h1>
+            </div>
+          </div>
+          {/* bottom description */}
+        </div>
+      </div>
     </div>
   );
 }
